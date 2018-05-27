1133100f019aa34f6e23456497c0123d1e88c2f4
rebs1337 <pawel.szczech47@gmail.com>
2018-05-28 01:52:39 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lab10lib1',
-    version='1.0',
+    version='1.1',
     packages= ['functions'],
     install_requires=['mysql-connector', 'python-dateutil'],
     url='https://github.com/rebs1337/lab10lib1',
