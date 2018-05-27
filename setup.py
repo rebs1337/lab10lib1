74909f58f48daca4935489f50e083005186b3258
rebs1337 <pawel.szczech47@gmail.com>
2018-05-28 01:14:17 +0200

    test1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lab10lib1',
+    version='1.0',
+    packages=['functions'],
+    url='https://github.com/rebs1337/lab10lib1',
+    license='MIT',
+    author='Paweł Szczęch',
+    author_email='226160@student.pwr.edu.pl',
+    description='-'
+)
